e52eccf9896f065ca795502ea95100cb847e6f34
home <home@DESKTOP-TIJENPU>
2017-11-28 16:50:03 +0900

    파이썬 실행시키는 파일 성공!!

---
+++ src/read/python/RunFile.java
@@ -0,0 +1,26 @@
+package read.python;
+
+import java.io.*;
+import java.util.*;
+
+public class RunFile {
+	public static void main(String args[]) throws IOException {
+
+		try {
+			Runtime r = Runtime.getRuntime();
+			Process p = r.exec("cmd /c C:\\Python32\\python.exe D:\\다운로드//script.py"); //cmd를 이용해 파이썬의 경로를 입력해 파이썬을 실행시키고 실행시킨 파이썬을 이용해 파이썬 파일안의 코드를 실행
+
+			BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			System.out.println(".........start   process.........");
+			String line = "";
+			while ((line = bfr.readLine()) != null) {
+				System.out.println("Python Output: " + line);
+			}
+			System.out.println("........end   process.......");
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
