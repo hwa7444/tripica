e88a19c3e96cd0eb982a26b01969bb699e5b75ad
wkizx <wkizx@naver.com>
2017-12-02 20:32:07 +0900

    뭐야이거

--- src/com/Service/deleteCon.java
+++ src/com/Service/deleteCon.java
@@ -9,11 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.DAO.bulletinDAO;
 
-
 @WebServlet("/deleteCon")
 public class deleteCon extends HttpServlet {
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		int num = Integer.parseInt(request.getParameter("num"));
 
 		bulletinDAO dao = bulletinDAO.getInstance();
@@ -23,11 +23,10 @@ public class deleteCon extends HttpServlet {
 
 			if (isSuccess) {
 				response.sendRedirect("SelectService?start=0&end=8");
-			}else {
+			} else {
 				// 삭제 실패 했을 때, 식패 알림 띄우기
 				System.out.println("삭제실패");
 			}
-			
 
 		} catch (
 
